a52b72961413ae479b04ecf655e07582ca3f913f
zh1 <zh1047049332@gmail.com>
2022-09-11 22:28:02 +0900

    test was a sample. changed huffman.

--- data_structure/huffman.c
+++ data_structure/huffman.c
@@ -1,4 +1,4 @@
-//建立最小堆和哈夫曼树
+//建立最小堆(链表)和哈夫曼树
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,19 +14,35 @@ struct TreeNode
 //最小堆的结构体声明
 typedef struct HeapStruct *MinHeap;
 struct HeapStruct{
-    int *Elements;
+    HuffmanTree *Elements;
     int Size;       //当前大小
     int Capacity;   //最大容量
 };
 
-MinHeap Creat( int MaxSize ){
+HuffmanTree NewNode( int data){
+    HuffmanTree TN;
 
-    MinHeap H = malloc(sizeof(struct HeapStruct));
+    TN = (HuffmanTree)malloc(sizeof( struct TreeNode));
+    TN->Left = TN->Right = NULL;
 
-    H->Elements = malloc((MaxSize + 1) * sizeof(int));         //从1开始存放数据
+    TN->Weight = data;
+
+    return TN;
+}
+
+MinHeap CreatMinHeap( int MaxSize ){
+
+    MinHeap H = (MinHeap)malloc(sizeof(struct HeapStruct));
+
+    H->Elements = (HuffmanTree*)malloc((MaxSize + 1) * sizeof(HuffmanTree));         //从1开始存放数据
     H->Size = 0;
     H->Capacity = MaxSize;
-    H->Elements[0] = -1;          //哨兵元素
+
+    HuffmanTree T = NewNode(0);
+
+    T->Weight = -1;     //设为最小值,作为哨兵元素
+
+    H->Elements[0] = T;          //把哨兵元素放到第一个位置
 
     return H;
 }
@@ -41,7 +57,7 @@ int IsEmpty(MinHeap H){
     else return 0;
 }
 
-void Insert( MinHeap H, int X){
+void Insert( MinHeap H, HuffmanTree X){
     int i;
 
     if ( Isfull(H)){
@@ -51,20 +67,20 @@ void Insert( MinHeap H, int X){
 
     i = ++H->Size; // i指向插入后堆的最后一个元素
 
-    for(; H->Elements[i/2] > X; i/=2){// i/2 是父节点，如果插入元素比父节点小
+    for(; H->Elements[i/2]->Weight > X->Weight; i/=2){// i/2 是父节点，如果插入元素比父节点小
         H->Elements[i] = H->Elements[i/2];
     }
     H->Elements[i] = X;
 }
 
-int DeleteMin(MinHeap H){
+HuffmanTree DeleteMin(MinHeap H){//从最小堆取出并删除值
     int Parent, Child;
-    int MinItem, temp;
+    HuffmanTree MinItem, temp = NULL;
 
 
     if( IsEmpty(H)){
         printf("empty");
-        return -1;
+        return NULL;
     }
 
     MinItem = H->Elements[1];
@@ -73,10 +89,10 @@ int DeleteMin(MinHeap H){
 
     for(Parent = 1; Parent*2 <= H->Size; Parent = Child){//如果2*parent大于size表示没有左儿子更没有右儿子，child=parent表示移动到小儿子并且继续比较
         Child = Parent*2;                                                  //左儿子，右儿子parent*2+1
-        if((Child != H->Size) && (H->Elements[Child] > H->Elements[Child +1])){//child等于size表示没有右儿子就不用比较
+        if((Child != H->Size) && (H->Elements[Child]->Weight > H->Elements[Child +1]->Weight)){//child等于size表示没有右儿子就不用比较
             Child++;   //child指向更小的儿子
         }
-        if(temp <= H->Elements[Child]) break;  //如果比小的儿子还要小表示找到位置
+        if(temp->Weight <= H->Elements[Child]->Weight) break;  //如果比小的儿子还要小表示找到位置
         else H->Elements[Parent] = H->Elements[Child];
     }//循环结束表示找到合适的位置
 
@@ -87,7 +103,7 @@ int DeleteMin(MinHeap H){
 
 void PercDown( MinHeap H, int p){//p表示最后一个元素
     int Parent, Child;
-    int X;
+    HuffmanTree X;
 
     X = H->Elements[p];
 
@@ -95,29 +111,35 @@ void PercDown( MinHeap H, int p){//p表示最后一个元素
 
         Child = Parent*2;
 
-        if((Child != H->Size) && (H->Elements[Child] > H->Elements[Child +1])){     //比较左子树和右子树大小,child表示小的儿子，如果child等于size表示只有左儿子没有右儿子不用参与比较
-            Child++;
+        if((Child != H->Size) && (H->Elements[Child]->Weight > H->Elements[Child +1]->Weight)){     //比较左子树和右子树大小,child表示小的儿子，如果child等于size表示只有左儿子没有右儿子不用参与比较
+            Child++;//儿子中较小的那个
         }
 
-        if( X <= H->Elements[Child]) break;          //比父节点小，交换位置
+        if( X->Weight <= H->Elements[Child]->Weight) break;          //比父节点小，交换位置
         else{
             H->Elements[Parent] = H->Elements[Child];
         }
         
     }
     H->Elements[Parent] = X;
+
 }
 
-void BuildHeap( MinHeap H, int *nums){
+void BuildMinHeap( MinHeap H, int *nums){
     int i;
+    HuffmanTree temp;
+    
 
-    for( i = 0; i < H->Capacity; i++){
-        H->Elements[i+1] = nums[i];
+    for( i = 0; i < H->Capacity/2; i++){
+        temp = NewNode(nums[i]);
+
+        H->Elements[i+1] = temp;
         H->Size++;
     }
 
     for (i = H->Size/2; i > 0; i--){
         PercDown(H,i);
+
     }
     
 }
@@ -126,9 +148,10 @@ void PrintHeap( MinHeap H){
     printf("-----打印堆----\n");
 
     for(int i = 1; i <= H->Size; i++){
-        printf("%d ",H->Elements[i]);
+        printf("%d ",H->Elements[i]->Weight);
         if((i == 1) || (i == 3) || (i == 7)) printf("\n");
     }
+    printf("\n");
 }
 
 HuffmanTree Huffman(MinHeap H, int* num){
@@ -136,11 +159,8 @@ HuffmanTree Huffman(MinHeap H, int* num){
     int i;
     HuffmanTree T;
 
-    H = Creat(10);
-    BuildHeap(H,nums);
-
     for( i = 1; i < H->Size; i++){
-        T = malloc(sizeof(struct TreeNode));
+        T = NewNode(0);
         T->Left = DeleteMin(H);
         T->Right = DeleteMin(H);
 
@@ -153,29 +173,36 @@ HuffmanTree Huffman(MinHeap H, int* num){
     return T;
 }
 
+void PreOrderTraversal(HuffmanTree BST){
+
+    if( BST ){
+
+        printf("%d ",BST->Weight);     //先访问根节点
+
+        PreOrderTraversal(BST->Left);  //再访问左子树
+
+        PreOrderTraversal(BST->Right); //最后访问右子树
+
+    }
+}
+
 void main(){
 
     MinHeap H;
     HuffmanTree T;
 
-    int nums[10] = {12,52,63,34,87,23,39,92,48,70};
+    int nums[4] = {1,5,2,6};
 
+    
+    H = CreatMinHeap(4*2);
+    BuildMinHeap(H,nums);
+    PrintHeap(H);
+    //printf("---删除元素---\n%d\n ",DeleteMin(H)->Right);
+    //PrintHeap(H);
     T = Huffman(H,nums);
+    printf("---先序遍历---");
+    PreOrderTraversal(T);
+
 
     
-
-    /*PrintHeap(H);
-    printf("\n");
-    
-    printf("删除元素：%d\n",DeleteMin(H));
-    PrintHeap(H);
-    printf("\n");
-
-    DeleteMin(H);
-    PrintHeap(H);
-    printf("\n");
-
-    Insert(H,55);
-    PrintHeap(H);*/
-
 }
--- data_structure/test.c
+++ data_structure/test.c
@@ -1,40 +1,366 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
 
+#include<stdlib.h>
+#include<stdbool.h>
 
-void merge_sort_recursive(int arr[], int reg[], int start, int end) {
-    if (start >= end)
-        return;
-    int len = end - start, mid = (len >> 1) + start;
-    int start1 = start, end1 = mid;
-    int start2 = mid + 1, end2 = end;
-    merge_sort_recursive(arr, reg, start1, end1);
-    merge_sort_recursive(arr, reg, start2, end2);
-    int k = start;
-    while (start1 <= end1 && start2 <= end2)
-        reg[k++] = arr[start1] < arr[start2] ? arr[start1++] : arr[start2++];
-    while (start1 <= end1)
-        reg[k++] = arr[start1++];
-    while (start2 <= end2)
-        reg[k++] = arr[start2++];
-    for (k = start; k <= end; k++)
-        arr[k] = reg[k];
-}
-void merge_sort(int arr[], const int len) {
-    int reg[len];
-    merge_sort_recursive(arr, reg, 0, len - 1);
-}
+#define MinData -1
 
+typedef struct TreeNode *HuffmanTree;//哈夫曼树类型
 
-main(){
-    int nums[] = {12,32,432,543,7,2,434,23,534,68};
+struct TreeNode{
 
-    merge_sort(nums,10);
+int Weight;  //结点权值
 
-    for(int i = 0; i<10; i++){
-        printf("%d  ",nums[i]);
+HuffmanTree Left;//指向左子树
+
+HuffmanTree Right;//指向右子树
+
+}HuffmanNode;//定义新的结构变量
+
+  
+
+typedef struct HeapStruct *MinHeap;
+
+struct HeapStruct{
+
+HuffmanTree *Data;  //存储堆元素的数组  存储时从下标1开始
+
+int Size;  //堆的当前元素的个数
+
+int Capacity;  //堆的最大容量
+
+};
+
+ 
+
+HuffmanTree NewHuffmanNode();
+
+//构造新的哈夫曼树
+
+MinHeap CreateMinHeap(int MaxSize);
+
+//创建容量为MaxSize的最小堆
+
+bool Insert(MinHeap H,HuffmanTree item);
+
+//将元素item插入到最小堆H中
+
+HuffmanTree DeleteMin(MinHeap H);
+
+//从最小堆H中取出权值为最小的元素，并删除一个结点
+
+MinHeap BuildMinHeap(MinHeap H);
+
+//将H->data[]按权值调整为最小堆
+
+HuffmanTree Huffman(MinHeap H);
+
+//最小堆构造哈夫曼树
+
+void PreOrderTraversal(HuffmanTree BST);
+
+//先序遍历哈夫曼树
+
+ 
+
+int main()
+
+{
+
+    int i,N;
+
+    MinHeap h;
+
+    HuffmanTree T,BT = NULL;
+
+ 
+
+    printf("输入叶子结点的个数:\n");
+
+    scanf("%d",&N);
+
+    h = CreateMinHeap(2*N);  //创建最小堆
+
+    printf("输入%d个叶子结点对应的权值:\n",N);
+
+    for(i=1; i<=N; i++){/*最小堆元素赋值*/
+
+        T = NewHuffmanNode();
+
+        scanf("%d",&(T->Weight));
+
+        h->Data[++(h->Size)] = T;
+
     }
 
+    BT = Huffman(h);  //构造哈夫曼树
 
+    printf("先序遍历此哈夫曼树:\n");
+
+    PreOrderTraversal(BT);  //先序遍历哈夫曼树
+
+ 
+
+    return 0;
+
+ }
+
+  /*哈夫曼树构造算法*/
+
+ HuffmanTree Huffman(MinHeap H)
+
+ {/*假设H->Size个权值已经存在H->data[]->Weight里*/
+
+  int i,n;
+
+HuffmanTree T;
+
+ 
+
+BuildMinHeap( H );  //将H->data[]按权值调整为最小堆
+
+/*此处必须将H->Size的值交给num,因为后面做DeleteMin()和 Insert()函数会改变H->Size的值*/
+
+n = H->Size;     
+
+for(i=1; i<n; i++){  //做 H->Size-1次合并
+
+    T = NewHuffmanNode();  //建立一个新的根结点
+
+    T->Left = DeleteMin(H);  //从最小堆中删除一个节点，作为新T的左子结点
+
+    T->Right = DeleteMin(H);  //从最小堆中删除一个节点，作为新T的右子结点
+
+    T->Weight = T->Left->Weight+T->Right->Weight;  //计算新权值
+
+    Insert(H,T);  //将新T插入到最小堆
+
+}
+
+T = DeleteMin(H);
+
+ 
+
+return T;
+
+  }
+
+  
+
+//先序遍历哈夫曼树
+
+void PreOrderTraversal(HuffmanTree BST)
+
+{
+
+if( BST ){
+
+printf("%d ",BST->Weight);     //先访问根节点
+
+PreOrderTraversal(BST->Left);  //再访问左子树
+
+PreOrderTraversal(BST->Right); //最后访问右子树
+
+}
+
+}
+
+ 
+
+HuffmanTree NewHuffmanNode()
+
+{
+
+HuffmanTree BST = (HuffmanTree)malloc(sizeof(HuffmanNode));
+
+BST->Weight = 0;
+
+BST->Left = BST->Right = NULL;
+
+ 
+
+return BST;
+
+ }  
+
+  
+
+MinHeap CreateMinHeap(int MaxSize)
+
+{  /*创建容量为MaxSize的最小堆*/
+
+MinHeap H = (MinHeap)malloc(sizeof(struct HeapStruct));
+
+H->Data = (HuffmanTree *)malloc((MaxSize+1) * sizeof(HuffmanTree));
+
+H->Size = 0;
+
+H->Capacity = MaxSize;
+
+HuffmanTree T = NewHuffmanNode();
+
+T->Weight = MinData;  /*定义哨兵-为小于堆中所有可能元素权值的值，便于以后更快操作*/
+
+H->Data[0] = T;
+
+ 
+
+return H;
+
+}
+
+ //插入新增节点
+
+bool  IsFull(MinHeap H)
+
+{
+
+return (H->Size == H->Capacity);
+
+}
+
+ 
+
+bool IsEmpty(MinHeap H)
+
+{
+
+return (H->Size == 0);
+
+}
+
+ 
+
+ 
+
+bool Insert(MinHeap H,HuffmanTree item)
+
+{        //将元素item插入到最小堆H中
+
+int i;
+
+if( IsFull(H) ){
+
+printf("最小堆已满\n");
+
+return false;
+
+}
+
+i = ++H->Size;  //i指向插入后堆中的最后一个元素的位置
+
+for(; H->Data[i/2]->Weight > item->Weight; i/=2)  //无哨兵，则增加判决条件 i>1
+
+    H->Data[i] = H->Data[i/2];  //向下过滤结点
+
+H->Data[i] = item;   //将item插入
+
+ 
+
+return true;
+
+ }
+
+ 
+
+HuffmanTree DeleteMin(MinHeap H)
+
+{/*从最小堆H中取出权值为最小的元素，并删除一个结点*/
+
+int parent,child;
+
+HuffmanTree MinItem,temp = NULL;
+
+if( IsEmpty(H) ){
+
+printf("最小堆为空\n");
+
+return NULL;
+
+}
+
+MinItem = H->Data[1];  //取出根结点-最小的元素-记录下来
+
+//用最小堆中的最后一个元素从根结点开始向上过滤下层结点
+
+temp = H->Data[H->Size--];  //最小堆中最后一个元素，暂时将其视为放在了根结点
+
+for(parent=1; parent*2<=H->Size; parent=child){
+
+child = parent*2;
+
+if((child != H->Size) && (H->Data[child]->Weight > H->Data[child+1]->Weight)){
+
+/*有右儿子，并且左儿子权值大于右儿子*/
+
+child++; //child指向左右儿子中较小者
+
+}
+
+if(temp->Weight > H->Data[child]->Weight){
+
+H->Data[parent] = H->Data[child];  //向上过滤结点-temp存放位置下移到child位置
+
+}else{
+
+break;  //找到了合适的位置
+
+}
+
+}
+
+H->Data[parent] = temp;  //temp存放到此处
+
+ 
+
+return MinItem;
+
+}
+
+ 
+
+MinHeap BuildMinHeap(MinHeap H)
+
+{
+
+int i,parent,child;
+
+HuffmanTree temp;
+
+for(i=H->Size/2;i>0;i--){  //从最后一个父结点开始，直到根结点
+
+temp = H->Data[i];
+
+for(parent=i; parent*2<=H->Size; parent=child){
+
+    /*向下过滤*/
+
+child = parent*2;
+
+    if((child != H->Size) && (H->Data[child]->Weight > H->Data[child+1]->Weight)){/*有右儿子，并且左儿子权值大于右儿子*/
+
+    child++; //child指向左右儿子中较小者
+
+    }
+
+    if(temp->Weight > H->Data[child]->Weight){
+
+    H->Data[parent] = H->Data[child];  //向上过滤结点-temp存放位置下移到child位置
+
+    }else{
+
+    break;  //找到了合适的位置
+
+    }
+
+    }/*结束内部for循环对以H->data[i]为根的子树的调整*/
+
+ 
+
+H->Data[parent] = temp;  //temp存放到此处  
+
+}
+
+return H;
 
 }
